64a41b5a9109d90e42fce6aa163c9498c9a56fea
Alex King <43203115+kin14270476@users.noreply.github.com>
2018-10-24 10:56:32 +0100

    added lives and updated text displayed

--- code.cpp
+++ code.cpp
@@ -7,52 +7,66 @@ using namespace std;
 
 int main() 
 {
+  cout << "In this game you will roll a dice and you must guess if you rolled higher or lower than the computer dice." << endl;
   int PA; //PA means play again?
   PA = 1;
   while(PA==1)
   {
-    srand(time(NULL));
+    int lives;
+    lives = 3;
 
-    int UserDice;
-    UserDice=rand() %10 + 1;
-    cout << UserDice << endl;
-
-    int ComputerDice;
-    ComputerDice=rand() %10 + 1;
-    cout << ComputerDice << endl;
-    
-    string HOL; //HOL means Higher Or Lower
-    cout << "higher or lower:" ; 
-    cin >> HOL;
-    cout << HOL<< endl;
-
-    if (UserDice < ComputerDice)
+    int wins;
+    wins = 0;
+    while(lives>0)
     {
-      if (HOL == "higher")
+      cout << endl;
+      srand(time(NULL));
+      
+      int UserDice;
+      UserDice=rand() %10 + 1;
+      cout << "You rolled a " << UserDice << "." << endl;
+
+      int ComputerDice;
+      ComputerDice=rand() %10 + 1;
+      cout << "The computer rolled a " << ComputerDice << " only for debugging." << endl;
+      
+      string HOL; //HOL means Higher Or Lower
+      cout << "Do you think you rolled higher or lower:" ; 
+      cin >> HOL;
+      cout << "User guessed " << HOL << " only for debugging." << endl;
+
+      if (UserDice < ComputerDice) //This figures out if the dice is lower and if it is then it will use the users input to decide if they won or not.
       {
-        cout << "you win";
+        if (HOL == "higher")
+        {
+          wins = wins + 1;
+          cout << "You guessed right." << endl << "You have won " << wins << " times and you have " << lives << " lives left." << endl;
+        }
+        else 
+        {
+          lives = lives - 1 ;
+          cout << "You guessed wrong." << endl << "You have " << lives << " lives left." << endl;
+        }
       }
-      else 
+      else if (UserDice > ComputerDice) //This figures out if the dice is higher and if it is then it will use the users input to decide if they won or not.
       {
-        cout << "you lose";
+        if (HOL == "lower")
+        {
+          wins = wins + 1;
+          cout << "You guess right." << endl << "You have won " << wins << " times and you have " << lives << " lives left." << endl;
+        }
+        else 
+        {
+          lives = lives - 1 ;
+          cout << "You guessed wrong." << endl << "you have " << lives << " lives left." << endl;
+        }
+      }
+      else //If the dice is not higher or lower it must be equal so the player draws
+      {
+        cout << "Your dice and the computer dice was the same so it is a draw." << endl;
       }
     }
-    else if (UserDice > ComputerDice)
-    {
-      if (HOL == "lower")
-      {
-        cout << "you win" << endl;
-      }
-      else 
-      {
-        cout << "you lose" << endl;
-      }
-    }
-    else
-    {
-      cout << "draw" << endl;
-    }
-    cout << "play again?(Yes/No):";
+    cout << endl << "Game Over." << endl << "you won " << wins << " times" << endl << "play again?(Yes/No):";
     string YON; // YON means Yes or No
     cin >> YON;
     if (YON == "No")
